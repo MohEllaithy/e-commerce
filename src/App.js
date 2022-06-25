ca1c0b2fb3111bf48e2ef10411c3fc7284f59ff3
MohEllaithy <ellaithy80@gmail.com>
2022-06-25 19:05:45 +0200

    Restore App and index Files

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
-import Director from "./components/directory/Directory";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
+import Home from "./routes/home/Home";
+import Navigation from "./routes/navigation/Navigation";
+import Authentication from "./routes/authentication/Authentication";
+import Shop from "./routes/shop/Shop";
+import Checkout from "./routes/checkout/Checkout";
 
-  return <Director categories={categories} />;
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
